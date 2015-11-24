e59c98e5c3b6309d48fe5e263f85f3ea826f989f
sizhiqiang <szq326532590@126.com>
2015-11-24 13:14:18 +0800

    change index.html and index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /**主页的js文件**/
 window.onload = function (){
-	alert('aa');
+	var body = document.getElementById("body");
+	body.style.background = red;
+	body.style.color = blue;
+	body.margin = "0px";
+	body.padding = "0px";
+	body.border = "solid 1px #ddd";
 }
